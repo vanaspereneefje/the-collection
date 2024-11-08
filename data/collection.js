b7268d376749a8321effc280d0f4321a72f463cc
Eefje van Asperen <eefjevanasperen@hotmail.com>
2024-11-08 16:47:50 +0100

    Created basic styling for detail page and added plot to data array

--- data/collection.js
+++ data/collection.js
@@ -6,6 +6,7 @@ const collection = [
         director: 'Hayao Miyazaki',
         releaseYear: '2004',
         summary: "Jealous of Sophie's closeness to Howl, a wizard, the Witch of Waste transforms her into an old lady. Sophie must find a way to break the spell with the help of Howl's friends, Calcifer and Markl.",
+        plot: 'plot',
         link: 'https://www.youtube.com/watch?v=iwROgK94zcM'
     },
     {
@@ -15,6 +16,7 @@ const collection = [
         director: 'Hiromasa Yonebayashi',
         releaseYear: '2014',
         summary: 'A distressed girl is compelled to live in the countryside by her foster parents. There, she befriends Marnie, a mysterious girl, who helps her confront the truth about her identity.',
+        plot: 'plot',
         link: 'https://www.youtube.com/watch?v=jjmrxqcQdYg'
       },
       {
@@ -24,6 +26,7 @@ const collection = [
         director: 'Hayao Miyazaki',
         releaseYear: '2013',
         summary: 'A lifelong love of flight inspires Japanese aviation engineer Jiro Horikoshi, whose storied career includes the creation of the A6M World War II fighter plane.',
+        plot: 'plot',
         link: 'https://www.youtube.com/watch?v=RzSpDgiF5y8'
       },
       {
@@ -33,6 +36,7 @@ const collection = [
         director: 'Hayao Miyazaki',
         releaseYear: '1984',
         summary: 'In a post-apocalyptic age, Nausicaa, a princess who guards the Valley of the Wind, tries to stop two warring nations from destroying everything around them.',
+        plot: 'plot',
         link: 'https://www.youtube.com/watch?v=6zhLBe319KE'
       },
       {
@@ -42,6 +46,7 @@ const collection = [
         director: 'Hayao Miyazaki',
         releaseYear: '1988',
         summary: "Two sisters relocate to rural Japan with their father to spend time with their ill mother. They face a mythical forest sprite and its woodland friends with whom they have many magical adventures.",
+        plot: 'plot',
         link: 'https://www.youtube.com/watch?v=6zhLBe319KE'
     },
     {
@@ -51,6 +56,7 @@ const collection = [
         director: 'Hiromasa Yonebayashi',
         releaseYear: '2010',
         summary: "The Clock family lives in another family's residence and steals their goods. All is well until humans discover their daughter, Arrietty, and they must flee for their lives.",
+        plot: 'plot',
         link: 'https://www.youtube.com/watch?v=6zhLBe319KE'
     },
     {
@@ -60,6 +66,7 @@ const collection = [
         director: 'Hayao Miyazaki',
         releaseYear: '1989',
         summary: "Thirteen-year-old Kiki tries to become an independent witch and gets a job at a delivery service. She wakes up one day to find that she can neither fly her broom nor talk to her cat.",
+        plot: 'plot',
         link: 'https://www.youtube.com/watch?v=6zhLBe319KE'
     },
     {
@@ -69,6 +76,7 @@ const collection = [
         director: 'Hayao Miyazaki',
         releaseYear: '1997',
         summary: "While saving his village, Ashitaka, a heroic warrior, is befallen with a curse. To lift the curse, Ashitaka embarks on a perilous journey and gets involved in the conflict of two warring clans.",
+        plot: 'plot',
         link: 'https://www.youtube.com/watch?v=6zhLBe319KE'
     },
     {
@@ -78,6 +86,7 @@ const collection = [
         director: 'Isao Takahata',
         releaseYear: '2013',
         summary: "A farmer and his wife find a miniature girl inside a bamboo stalk who magically grows up to become a beautiful lady. The girl is courted by five men who have to face impossible tasks to win her hand.",
+        plot: 'plot',
         link: 'https://www.youtube.com/watch?v=6zhLBe319KE'
     },
     {
@@ -87,6 +96,7 @@ const collection = [
         director: 'Hayao Miyazaki',
         releaseYear: '2001',
         summary: "Ten-year-old Chihiro and her parents end up at an abandoned amusement park inhabited by supernatural beings. Soon, she learns that she must work to free her parents who have been turned into pigs.",
+        plot: 'plot',
         link: 'https://www.youtube.com/watch?v=6zhLBe319KE'
     },
   ]
--- detail.js
+++ detail.js
@@ -10,32 +10,32 @@ let containerDiv = document.createElement("div");
 document.querySelector("main").appendChild(containerDiv)
 containerDiv.classList.add("container");
 
-let picture = document.createElement("img");
-picture.src = collection[articleId].picture;
-containerDiv.appendChild(picture);
-picture.classList.add("image");
-picture.style="width:100%";
-
 let title = document.createElement("h2");
 containerDiv.appendChild(title);
 let nameNode = document.createTextNode(collection[articleId].name);
 title.appendChild(nameNode);
-title.classList.add("card-title");
+title.classList.add("title");
+
+let picture = document.createElement("img");
+picture.src = collection[articleId].picture;
+containerDiv.appendChild(picture);
+picture.classList.add("image");
 
 let director = document.createElement("p");
 containerDiv.appendChild(director);
 let directorNode = document.createTextNode(collection[articleId].director);
 director.appendChild(directorNode);
-director.classList.add("card-director");
+director.classList.add("director");
 
 let release = document.createElement("p");
 containerDiv.appendChild(release);
 let releaseNode = document.createTextNode(collection[articleId].releaseYear);
+director.classList.add("release");
 release.appendChild(releaseNode);
 
 let summary = document.createElement("p");
 containerDiv.appendChild(summary);
-let summaryNode = document.createTextNode(collection[articleId].summary);
+let summaryNode = document.createTextNode(collection[articleId].plot);
 summary.appendChild(summaryNode);
 summary.classList.add("text");
 });
